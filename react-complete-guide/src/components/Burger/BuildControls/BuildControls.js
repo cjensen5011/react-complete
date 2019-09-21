cae8404637986342ff7deb9b4bfc8379de139659
Christopher Jensen <christopher.jensen@daugherty.com>
2019-09-21 08:51:43 -0500

    burger updates

--- react-complete-guide/src/components/Burger/BuildControls/BuildControls.js
+++ react-complete-guide/src/components/Burger/BuildControls/BuildControls.js
@@ -11,10 +11,17 @@ const controls = [
 
 const buildControls = props => (
   <div className={classes.BuildControls}>
+    <p>Current Price: {props.price}</p>
     {controls.map(ctrl => (
-      <BuildControl key={ctrl.label} label={ctrl.label} />
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
     ))}
-    <button className={classes.OrderButton}>Order Now!</button>
+    {/* <button className={classes.OrderButton}>Order Now!</button> */}
   </div>
 );
 
